670bdea3d58d5ec299e5fd38642d6dcdb9dffa82
jmp7911 <jmp7911@gmail.com>
2020-04-25 16:14:11 +0900

    Stadium - add Stadium

--- Dailymlb/app/src/main/java/com/jmp/dailymlb/model/Stadium.java
+++ Dailymlb/app/src/main/java/com/jmp/dailymlb/model/Stadium.java
@@ -20,47 +20,23 @@ public class Stadium {
         return stadiumId;
     }
 
-    public void setStadiumId(int stadiumId) {
-        this.stadiumId = stadiumId;
-    }
-
     public boolean isActive() {
         return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getCity() {
         return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
     }
 
     public String getState() {
         return state;
     }
 
-    public void setState(String state) {
-        this.state = state;
-    }
-
     public String getCountry() {
         return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
     }
 }
