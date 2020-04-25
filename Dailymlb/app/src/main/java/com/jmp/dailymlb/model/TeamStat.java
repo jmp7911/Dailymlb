1e0dbd4131f2db179b39c5b47dc09edfd3e39191
jmp7911 <jmp7911@gmail.com>
2020-04-25 16:15:07 +0900

    TeamStat - add TeamStat

--- Dailymlb/app/src/main/java/com/jmp/dailymlb/model/TeamStat.java
+++ Dailymlb/app/src/main/java/com/jmp/dailymlb/model/TeamStat.java
@@ -26,71 +26,35 @@ public class TeamStat {
         return statId;
     }
 
-    public void setStatId(int statId) {
-        this.statId = statId;
-    }
-
     public int getSeasonType() {
         return seasonType;
-    }
-
-    public void setSeasonType(int seasonType) {
-        this.seasonType = seasonType;
     }
 
     public int getSeason() {
         return season;
     }
 
-    public void setSeason(int season) {
-        this.season = season;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getTeam() {
         return team;
     }
 
-    public void setTeam(String team) {
-        this.team = team;
-    }
-
     public String getUpdated() {
         return updated;
-    }
-
-    public void setUpdated(String updated) {
-        this.updated = updated;
     }
 
     public int getGames() {
         return games;
     }
 
-    public void setGames(int games) {
-        this.games = games;
-    }
-
     public float getWins() {
         return wins;
     }
 
-    public void setWins(float wins) {
-        this.wins = wins;
-    }
-
     public float getLosses() {
         return losses;
-    }
-
-    public void setLosses(float losses) {
-        this.losses = losses;
     }
 }
