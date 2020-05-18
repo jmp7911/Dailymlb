46747119552f701c61f4fb4c2f572e465de208fd
jmp7911 <jmp7911@gmail.com>
2020-05-18 22:55:16 +0900

    MainView -

--- Dailymlb/app/src/main/java/com/jmp/dailymlb/model/GameReviewWrapper.java
+++ Dailymlb/app/src/main/java/com/jmp/dailymlb/model/GameReviewWrapper.java
@@ -20,30 +20,13 @@ public class GameReviewWrapper {
     TextView homeStat;
 
     TableLayout tableBoard;
-    TableLayout tableStat;
     TableLayout tableHit;
     TableLayout tableHomeRun;
     TableLayout tableStolenBase;
     TableLayout tablePitcherStrikeOut;
     TableLayout tableError;
 
-    TextView awayHitOnStat;
-    TextView homeHitOnStat;
-    TextView awayHomeRunOnStat;
-    TextView homeHomeRunOnStat;
-    TextView awayStolenBaseOnStat;
-    TextView homeStolenBaseOnStat;
-    TextView awayPitcherStrikeOut;
-    TextView homePitcherStrikeOut;
-    TextView awayErrorOnStat;
-    TextView homeErrorOnStat;
-    TextView hitOnStat;
-    TextView homeRunOnStat;
-    TextView stolenBaseOnStat;
-    TextView pitcherStrikeOutOnStat;
-    TextView errorOnStat;
     public GameReviewWrapper(View root) {
-        tableStat = root.findViewById(R.id.review_table_statistics);
         tableBoard = root.findViewById(R.id.review_table_board);
         tableHit = root.findViewById(R.id.review_table_hit);
         tableHomeRun = root.findViewById(R.id.review_table_home_run);
@@ -59,89 +42,10 @@ public class GameReviewWrapper {
         homeIcon = root.findViewById(R.id.review_home_icon);
         homeRank = root.findViewById(R.id.home_team_rank);
         homeStat = root.findViewById(R.id.home_team_stat);
-        awayHitOnStat = root.findViewById(R.id.statistics_away_hit);
-        homeHitOnStat = root.findViewById(R.id.statistics_home_hit);
-        awayHomeRunOnStat = root.findViewById(R.id.statistics_away_home_run);
-        homeHomeRunOnStat = root.findViewById(R.id.statistics_home_home_run);
-        awayStolenBaseOnStat = root.findViewById(R.id.statistics_away_stolen_base);
-        homeStolenBaseOnStat = root.findViewById(R.id.statistics_home_stolen_base);
-        awayPitcherStrikeOut = root.findViewById(R.id.statistics_away_pitcher_strike_out);
-        homePitcherStrikeOut = root.findViewById(R.id.statistics_home_pitcher_strike_out);
-        awayErrorOnStat = root.findViewById(R.id.statistics_away_error);
-        homeErrorOnStat = root.findViewById(R.id.statistics_home_error);
-        hitOnStat = root.findViewById(R.id.statistics_hit);
-        homeRunOnStat = root.findViewById(R.id.statistics_home_run);
-        stolenBaseOnStat = root.findViewById(R.id.statistics_stolen_base);
-        pitcherStrikeOutOnStat = root.findViewById(R.id.statistics_pitcher_strike_out);
-        errorOnStat = root.findViewById(R.id.statistics_error);
-    }
-
-    public TableLayout getTableStat() {
-        return tableStat;
     }
 
     public TableLayout getTableBoard() {
         return tableBoard;
-    }
-
-    public TextView getHitOnStat() {
-        return hitOnStat;
-    }
-
-    public TextView getHomeRunOnStat() {
-        return homeRunOnStat;
-    }
-
-    public TextView getStolenBaseOnStat() {
-        return stolenBaseOnStat;
-    }
-
-    public TextView getPitcherStrikeOutOnStat() {
-        return pitcherStrikeOutOnStat;
-    }
-
-    public TextView getErrorOnStat() {
-        return errorOnStat;
-    }
-
-    public TextView getAwayHitOnStat() {
-        return awayHitOnStat;
-    }
-
-    public TextView getHomeHitOnStat() {
-        return homeHitOnStat;
-    }
-
-    public TextView getAwayHomeRunOnStat() {
-        return awayHomeRunOnStat;
-    }
-
-    public TextView getHomeHomeRunOnStat() {
-        return homeHomeRunOnStat;
-    }
-
-    public TextView getAwayStolenBaseOnStat() {
-        return awayStolenBaseOnStat;
-    }
-
-    public TextView getHomeStolenBaseOnStat() {
-        return homeStolenBaseOnStat;
-    }
-
-    public TextView getAwayPitcherStrikeOut() {
-        return awayPitcherStrikeOut;
-    }
-
-    public TextView getHomePitcherStrikeOut() {
-        return homePitcherStrikeOut;
-    }
-
-    public TextView getAwayErrorOnStat() {
-        return awayErrorOnStat;
-    }
-
-    public TextView getHomeErrorOnStat() {
-        return homeErrorOnStat;
     }
 
     public TableLayout getTableHit() {
